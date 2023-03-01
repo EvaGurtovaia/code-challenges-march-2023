adb637ce36c1933ffe72b21cfca02091044cb39f
EvaGurtovaia <eva.gurtovaya@gmail.com>
2023-03-01 10:15:06 -0600

    Exercise: Array Methods

---
+++ weekFirst.js
@@ -0,0 +1,10 @@
+// Exercise: Array Methods
+// Your task is to add the missing logic to a transformToObjects() function that transforms a list of numbers into a list of JavaScript objects.
+
+// For the provided input [1, 2, 3] the transformToObjects() function should return [{val: 1}, {val: 2}, {val: 3}]
+
+function transformToObjects(numberArray) {
+    return numberArray.map((num) => ({ val: num }));
+}
+
+console.log(transformToObjects([1, 2, 3]));
